38715d26850f5f3b214246073b147f8bb1f326db
pasqualini9@gmail.com <pasqualini9@gmail.com@37c919ed-7cac-9a1e-5fd3-759e1a7ac0dd>
2012-02-23 23:01:38 +0000

    implementado média

--- trunk/Imagem/src/imagem/Formulario.java
+++ trunk/Imagem/src/imagem/Formulario.java
@@ -1,6 +1,7 @@
 package imagem;
 
 import java.awt.image.BufferedImage;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,14 +10,21 @@ import javax.swing.JPanel;
 public class Formulario {
     
     private BufferedImage imagem;
+    private BufferedImage imagemResultado;
     private ImageIcon icon;
+    private ImageIcon iconResultado;
     
     public void exibir(){
         icon =  new ImageIcon(imagem);
+        iconResultado =  new ImageIcon(imagemResultado);
         JFrame f = new JFrame("Imagem");
+        JFrame fR = new JFrame("Imagem Resultado");
         JPanel p = new JPanel();
+        JPanel pR = new JPanel();
         JLabel lblImagem = new JLabel();
+        JLabel lblImagemResultado = new JLabel();
         lblImagem.setIcon(icon);
+        lblImagemResultado.setIcon(iconResultado);
         
         p.add(lblImagem);
         f.add(p);
@@ -24,11 +32,23 @@ public class Formulario {
         f.pack();
         f.setVisible(true);
         
+        pR.add(lblImagemResultado);
+        fR.add(pR);
+        fR.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fR.pack();
+        fR.setVisible(true);
+        
     }
 
     public void setImagem(BufferedImage imagem) {
         this.imagem = imagem;
     }
+
+	public void setImagemResultado(BufferedImage imagemResultado) {
+		this.imagemResultado = imagemResultado;
+	}
+    
+    
 
     
     
--- trunk/Imagem/src/imagem/Imagem.java
+++ trunk/Imagem/src/imagem/Imagem.java
@@ -9,21 +9,25 @@ public class Imagem {
         System.out.println();
         Img i = new Img();
         //Do arquivo para o buffer para a matriz
-        i.lerArquivo("Imagem.jpg");
+        i.lerArquivo("imagem2.jpg");
         //Fara modificacoes
         //Soma os ponstos e tira a media 
-        i.cinza();
+       // i.cinza();
         //i.brilho(0);
         //i.contraste(0);
-       // i.negativo();
-       i.limiar_threshould(127);
+        //i.negativo();
+      // i.limiar_threshould(151);
+     //  i.limiar_threshould_inverso(127);
+        i.media();
         i.densidade();
         //Da matriz para o buffer
         i.lerMatriz();
-        //i.salvar(i.getImagem());
+        i.lerMatrizResultado();
+       // i.salvar(i.getImagem());
 
         Formulario f = new Formulario();
         f.setImagem(i.getImagem());
+        f.setImagemResultado(i.getImagemResultado());
         f.exibir();
 
         System.out.println("Demorou " + (System.currentTimeMillis() - agora));
