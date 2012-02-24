9a6ee14540b7d23b4eba2d399e0192d9a9fdb8ec
pasqualini9@gmail.com <pasqualini9@gmail.com@37c919ed-7cac-9a1e-5fd3-759e1a7ac0dd>
2012-02-24 22:05:25 +0000

    Mediana (Faltou limpar o ArrayList por isso que demorou)

--- Imagem/src/imagem/Imagem.java
+++ Imagem/src/imagem/Imagem.java
@@ -9,21 +9,22 @@ public class Imagem {
         System.out.println();
         Img i = new Img();
         //Do arquivo para o buffer para a matriz
-        i.lerArquivo("imagem2.jpg");
+        i.lerArquivo("imagem.jpg");
         //Fara modificacoes
         //Soma os ponstos e tira a media 
-       // i.cinza();
+        //i.cinza();
         //i.brilho(0);
         //i.contraste(0);
         //i.negativo();
-      // i.limiar_threshould(151);
-     //  i.limiar_threshould_inverso(127);
-        i.media();
+        //i.limiar_threshould(151);
+        //i.limiar_threshould_inverso(127);
+        //i.media();
+        i.mediana();
         i.densidade();
         //Da matriz para o buffer
         i.lerMatriz();
         i.lerMatrizResultado();
-       // i.salvar(i.getImagem());
+        //i.salvar(i.getImagem());
 
         Formulario f = new Formulario();
         f.setImagem(i.getImagem());
