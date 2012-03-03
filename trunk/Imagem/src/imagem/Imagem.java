4596661561ffb60f870348dbda762b93aa42ff70
pasqualini9@gmail.com <pasqualini9@gmail.com@37c919ed-7cac-9a1e-5fd3-759e1a7ac0dd>
2012-03-03 20:53:10 +0000

    Erosão e Dilatação

--- trunk/Imagem/src/imagem/Imagem.java
+++ trunk/Imagem/src/imagem/Imagem.java
@@ -9,17 +9,17 @@ public class Imagem {
         System.out.println();
         Img i = new Img();
         //Do arquivo para o buffer para a matriz
-        i.lerArquivo("carro_menos_8.jpg");
+        i.lerArquivo("carro_menos_8_dilatacao.jpg");
         //Fara modificacoes
         //Soma os ponstos e tira a media 
-        i.cinza();
+        //i.cinza();
         //i.cinzaResultado();
         //i.brilho(80);
         //i.contraste(3);
         //i.negativo();
         //i.limiar_threshould(127);
         //i.limiar_threshould_inverso(127);
-        i.media();
+        //i.media();
         //i.mediana();
         //i.sobel();
         //i.prewitt();
@@ -27,14 +27,19 @@ public class Imagem {
         //i.laplaciano_menos_8();
         //i.laplaciano_4();
         //i.laplaciano_menos_4();
-        i.limiar_threshould_resultado(70);
-        i.negativoResultado();
+        //i.limiar_threshould_resultado(100);
+        
+        i.erosao();
+        //i.dilataco();
+
+        
+        //i.negativoResultado();
         i.densidade();
         i.densidadeResultado();
         //Da matriz para o buffer
         i.lerMatriz();
         i.lerMatrizResultado();
-        //i.salvar(i.getImagemResultado(), "carro_menos_8");
+        //i.salvar(i.getImagemResultado(), "carro_menos_8_dilatacao");
 
         Formulario f = new Formulario();
         f.setImagem(i.getImagem());
