eab99a7b1406fa1990c9c3b09f3a72d17378d0c5
pasqualini9@gmail.com <pasqualini9@gmail.com@37c919ed-7cac-9a1e-5fd3-759e1a7ac0dd>
2012-03-17 19:09:37 +0000

    Inicio do desnvolvimento dos gráficos. Adicionado jar do jfreechart no buildpath

--- Imagem/src/imagem/Formulario.java
+++ Imagem/src/imagem/Formulario.java
@@ -2,6 +2,7 @@ package imagem;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 
@@ -15,23 +16,42 @@ public class Formulario {
     
     private BufferedImage imagem;
     private BufferedImage imagemResultado;
+    private BufferedImage histograma;
+    private BufferedImage projecaoVertical;
+    private BufferedImage projecaoHorizontal;
     private ImageIcon icon;
     private ImageIcon iconResultado;
+    private ImageIcon iconProjecaoHorizontal;
+    private ImageIcon iconProjecaoVertical;
+    private ImageIcon iconHistograma;
     
     public void exibir(){
         icon =  new ImageIcon(imagem);
         iconResultado =  new ImageIcon(imagemResultado);
+        iconProjecaoHorizontal = new ImageIcon(projecaoHorizontal);
+        iconProjecaoVertical = new ImageIcon(projecaoVertical);
+        iconHistograma = new ImageIcon(histograma);
         JFrame f = new JFrame("Imagem");
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         //System.out.println(d.width+"x"+d.height);
         JPanel p = new JPanel();
         JLabel lblImagem = new JLabel();
         JLabel lblImagemResultado = new JLabel();
+        JLabel lblProjecaoHorizontal = new JLabel();
+        JLabel lblProjecaoVertical = new JLabel();
+        JLabel lblHistograma = new JLabel();
         lblImagem.setIcon(icon);
         lblImagemResultado.setIcon(iconResultado);
+        lblProjecaoHorizontal.setIcon(iconProjecaoHorizontal);
+        lblProjecaoVertical.setIcon(iconProjecaoVertical);
+        lblHistograma.setIcon(iconHistograma);
+        
         
         p.add(lblImagem, BorderLayout.WEST);
         p.add(lblImagemResultado, BorderLayout.EAST);
+        p.add(lblProjecaoHorizontal, BorderLayout.SOUTH);
+        p.add(lblProjecaoVertical, BorderLayout.SOUTH);
+        p.add(lblHistograma, BorderLayout.SOUTH);
         f.add(p);
         
         JScrollPane scrollBar=new JScrollPane(p,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
@@ -49,10 +69,19 @@ public class Formulario {
     public void setImagemResultado(BufferedImage imagemResultado) {
             this.imagemResultado = imagemResultado;
     }
-    
-    
 
+	public void setHistograma(BufferedImage histograma) {
+		this.histograma = histograma;
+	}
+
+	public void setProjecaoVertical(BufferedImage projecaoVertical) {
+		this.projecaoVertical = projecaoVertical;
+	}
+
+	public void setProjecaoHorizontal(BufferedImage projecaoHorizontal) {
+		this.projecaoHorizontal = projecaoHorizontal;
+	}
     
     
-    
+   
 }
--- Imagem/src/imagem/Imagem.java
+++ Imagem/src/imagem/Imagem.java
@@ -12,9 +12,11 @@ public class Imagem {
 
     public static void main(String[] args) throws IOException {
         long agora = System.currentTimeMillis();
-        String arquivo = "objetos.jpg";
+        String arquivo = "lego1.jpg";
+       
     	BufferedImage imagem = ImageIO.read(new File(arquivo));
         BufferedImage imagemResultado = ImageIO.read(new File(arquivo));
+       
         int[][][] matriz;
         Img i = new Img();
         //Do arquivo para o buffer para a matriz
@@ -27,9 +29,24 @@ public class Imagem {
         
         
         int[][] matrizRegioes = new int[i.getLargura()][i.getAltura()];
-        i.zerarMatriz(matrizRegioes);
-        //i.cinza(matriz, matriz);
-        i.limiar_threshould(50, matriz, matriz);
+        //i.zerarMatriz(matrizRegioes);
+        //i.media(matriz, matriz);
+        i.contraste(3, matriz, matriz);
+        i.cinza(matriz, matriz);
+        //i.media(matriz, matriz);
+        i.limiar_threshould(240, matriz, matriz);
+        
+        
+        //i.fechamento(matriz, matriz);
+        //i.sobel(matriz, matriz);
+        //i.limiar_threshould(80, matriz, matriz);
+        //i.negativo(matriz, matriz);
+        //i.histograma(matriz);
+        //i.dilatacao(matriz, matriz);
+        //i.media(matriz, matriz);
+        //i.dilatacao(matriz, matriz);
+        i.erosao(matriz, matriz);
+        i.media(matriz, matriz);
         i.vizinhanca_4(matriz, matrizRegioes);
         
         
@@ -37,14 +54,29 @@ public class Imagem {
         //i.negativo(matrizResultado, matrizResultado);
         //i.densidade(matriz);
         //i.densidade(matrizResultado);
+        int[][] projecaoHorizontal = new int[i.getLargura()][i.getAltura()];
+        BufferedImage grficoProjecaoHorizontal = new BufferedImage(i.getLargura(), i.getAltura(), BufferedImage.TYPE_INT_RGB );
+        i.projecaoHorizontal(matriz, projecaoHorizontal);
+        int[][] projecaoVertical = new int[i.getLargura()][i.getAltura()];
+        BufferedImage grficoProjecaoVertical = new BufferedImage(i.getLargura(), i.getAltura(), BufferedImage.TYPE_INT_RGB );
+        i.projecaoVertical(matriz, projecaoVertical);
+        int[][] histograma = new int[256][101];
+        BufferedImage graficoHistograma = new BufferedImage(256, 101, BufferedImage.TYPE_INT_RGB );
+        i.histograma(matriz, histograma);
         //Da matriz para o buffer
         i.lerMatriz(matriz, imagem);
         i.lerMatriz(matrizResultado, imagemResultado);
+        i.lerMatriz(projecaoHorizontal, grficoProjecaoHorizontal);
+        i.lerMatriz(projecaoVertical, grficoProjecaoVertical);
+        i.lerHistograma(histograma, graficoHistograma);
         //i.salvar(i.getImagemResultado(), "carro_menos_8_erosao2");
 
         Formulario f = new Formulario();
         f.setImagem(imagem);
         f.setImagemResultado(imagemResultado);
+        f.setProjecaoHorizontal(grficoProjecaoHorizontal);
+        f.setProjecaoVertical(grficoProjecaoVertical);
+        f.setHistograma(graficoHistograma);
         f.exibir();
 
         System.out.println("Demorou " + (System.currentTimeMillis() - agora));
