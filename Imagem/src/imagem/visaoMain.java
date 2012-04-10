d6c9e645543e0b19134b33462fead51eac8f0cc6
pasqualini9@gmail.com <pasqualini9@gmail.com@37c919ed-7cac-9a1e-5fd3-759e1a7ac0dd>
2012-04-10 12:29:55 +0000

    Inicio da equalização do histograma

---
+++ Imagem/src/imagem/visaoMain.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagem;
+
+import processadoresImagem.Visao;
+
+/**
+ *
+ * @author rm61828
+ */
+public class visaoMain {
+   public static void main(String[] args){
+       Visao visao = new Visao();
+       FormularioVisao f = new FormularioVisao();
+       
+       int[][] imagem = visao.arquivo_Matriz("imagem.jpg");
+       f.setImagem(visao.matriz_Imagem(imagem));
+       visao.binariza(imagem, 127);
+       f.setImagemResultado(visao.matriz_Imagem(imagem));
+       
+       f.exibir();
+       f.getJFrame().setVisible(true);
+   }
+    
+    
+}
